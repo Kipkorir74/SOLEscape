33a6b6951745e59dbdb4eb3f0ca1e656aa1a3a67
Kipkorir74 <grahamhim74@gmail.com>
2023-08-29 13:02:29 +0300

    project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+import Hero from './sections/Hero';
+import SuperQuality from './sections/SuperQuality';
+import Services from './sections/Services';
+import CustomerReviews from './sections/CustomerReviews';
+import Subscribe from './sections/Subscribe';
+import PopularProducts from './sections/PopularProducts';
+import SpecialOffer from './sections/SpecialOffer';
+import Footer from './sections/Footer';
+import Nav from './Components/Nav.jsx';
+
+const App = () => (
+  <main className="relative">
+    <Nav />
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding-x padding-y">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffer />
+    </section>
+    <section className="bg-pale-blue padding">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="bg-black padding-x padding-y padding-t pb-8">
+      <Footer />
+    </section>
+
+  </main>
+)
+export default App;
